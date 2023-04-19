f46396f6fd2c90293dc82f7f0991ab422c2698c6
ozen-hub <info.ozenhub@gmail.com>
2023-04-19 22:48:21 +0530

    9

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -22,7 +22,12 @@ public class AppInitializer {
         try(Session session = HibernateUtil.getSessionFactory().openSession()){
             Student selectedStudent = session.find(Student.class,id);
             System.out.println(selectedStudent);
+            System.out.println("===========");
+            selectedStudent.getAddressList().stream()
+                    .forEach(e-> System.out.println(e.toString()));
+            System.out.println("===========");
         }
+
     }
 
     private static void saveStudent(Student sanath) {
