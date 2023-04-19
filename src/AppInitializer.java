b01278a67766457d47a6f5fd5caa5e6ff8f78399
ozen-hub <info.ozenhub@gmail.com>
2023-04-19 20:46:35 +0530

    1

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,6 @@
 import entity.Student;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -9,7 +10,9 @@ public class AppInitializer {
 
     private static void saveStudent(Student sanath) {
         try(Session session= HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
             long savedId = (long) session.save(sanath);
+            transaction.commit();
             System.out.println("saved Student Primary Key : "+savedId);
         }
     }
