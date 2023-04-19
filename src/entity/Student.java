2e858696d55b92e73bce107aec196bba82fd1f1d
ozen-hub <info.ozenhub@gmail.com>
2023-04-19 21:09:25 +0530

    2

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -5,19 +5,29 @@ import javax.persistence.*;
 @Entity(name = "student_table")
 public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private long studentId;
     @Column(name = "student_name", length = 50, nullable = false)
     private String name;
 
+    @Column(length = 45, nullable = false)
+    private String country;
+    @Column(length = 45, nullable = false)
+    private String city;
+    private int postal;
+
     public Student() {
     }
 
-    public Student(long studentId, String name) {
+    public Student(long studentId, String name, String country, String city, int postal) {
         this.studentId = studentId;
         this.name = name;
+        this.country = country;
+        this.city = city;
+        this.postal = postal;
     }
+
 
     public long getStudentId() {
         return studentId;
@@ -35,11 +45,27 @@ public class Student {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "studentId=" + studentId +
-                ", name='" + name + '\'' +
-                '}';
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostal() {
+        return postal;
+    }
+
+    public void setPostal(int postal) {
+        this.postal = postal;
     }
 }
